253d8f27265f9cef646cdc306dd0ad9369a31a65
MarkCodering <mark.chen.sstm@gmail.com>
2023-05-18 08:21:42 +0800

    update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,16 +10,12 @@ const Home: NextPage = () => {
       <Head>
         <title>DragonX Status Page</title>
       </Head>
-      <div className="mt-20 absolute inset-0 bg-[url(/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+      <div className="mt-20 absolute inset-0  bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
       <div className="w-full h-40 absolute bg-light-purple dark:purple dark:bg-black">
         <div className="sm:ml-0 ml-5 mr-0 mt-3 md:pl-80 md:pr-80 sm:w-full h-full bg-purple-500 dark:bg-black">
-          <Image
-            src="/vercel.svg"
-            width={150}
-            height={100}
-            className="w-40 h-16"
-            alt="Tailwind Play"
-          />
+          <h1 className="text-4xl font-bold text-white dark:text-white pt-6">
+            DragonX - DragonNote Status Page
+          </h1>
         </div>
       </div>
       <div className="mt-20 w-full absolute overflow-scroll	">
@@ -30,3 +26,4 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+//bg-[url(/grid.svg)]
